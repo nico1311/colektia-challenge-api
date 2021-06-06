69b0c7e9cb84952842913369fc77f9a6dc856541
nico1311 <71448116+nico1311@users.noreply.github.com>
2021-06-06 01:15:23 -0300

    Implemented read/update/delete operations

--- src/controllers/ProductsController.js
+++ src/controllers/ProductsController.js
@@ -18,7 +18,7 @@ const Product = require('../db/models/Product');
  *            $ref: '#/components/schemas/Product'
  *    responses:
  *      '201':
- *        description: 'Product successfully created'
+ *        description: 'Product created successfully'
  *        content:
  *          application/json:
  *            schema:
@@ -51,8 +51,7 @@ const createProduct = async (req, res) => {
     const product = await Product.create(productValues);
     res.status(201).json(product);
   } catch (err) {
-    console.error('validation error', err);
-    if (err.details) {
+    if (err.details) { // body validation error
       res.status(422).json({
         errors: err.details
       });
@@ -62,9 +61,215 @@ const createProduct = async (req, res) => {
       });
     }
   }
+}
 
+/**
+ * Get all products
+ * @swagger
+ * /api/products:
+ *  get:
+ *    tags:
+ *      - Products
+ *    summary: Get all products
+ *    responses:
+ *      '200':
+ *        description: 'Success'
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: array
+ *              description: 'Array of products'
+ *              items:
+ *                $ref: '#/components/schemas/Product'
+ * @param {import('express').Request} req
+ * @param {import('express').Response} res
+ * @returns {Promise<void>}
+ *
+ */
+const getAllProducts = async (req, res) => {
+  try {
+    const products = await Product.findAll();
+
+    res.status(200).json({
+      products
+    });
+
+  } catch (err) {
+    res.status(500).json({
+      error: err.message
+    })
+  }
+}
+
+/**
+ * Get a single product
+ * @swagger
+ * /api/products/{id}:
+ *  get:
+ *    tags:
+ *      - Products
+ *    summary: Get a single product
+ *    parameters:
+ *      - in: path
+ *        name: id
+ *        schema:
+ *          type: integer
+ *        required: true
+ *        description: 'ID of the product to get'
+ *    responses:
+ *      '200':
+ *        description: 'Success'
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/Product'
+ *      '404':
+ *        description: 'The requested product cannot be found'
+ * @param {import('express').Request} req
+ * @param {import('express').Response} res
+ * @returns {Promise<void>}
+ *
+ */
+const getProduct = async (req, res) => {
+  try {
+    const productId = parseInt(req.params.id, 10);
+    if (Number.isNaN(productId)) return res.status(404).end();
+
+    const product = await Product.findByPk(productId);
+    if (!product) return res.status(404).end();
+
+    res.status(200).json(product);
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+/**
+ * Edit a product
+ * @swagger
+ * /api/products/{id}:
+ *  patch:
+ *    tags:
+ *      - Products
+ *    summary: Edit a product
+ *    parameters:
+ *      - in: path
+ *        name: id
+ *        schema:
+ *          type: integer
+ *        required: true
+ *        description: 'ID of the product to edit'
+ *    requestBody:
+ *      required: true
+ *      content:
+ *        multipart/form-data:
+ *          schema:
+ *            $ref: '#/components/schemas/Product'
+ *    responses:
+ *      '200':
+ *        description: 'Product edited successfully'
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/Product'
+ *      '404':
+ *        description: 'The requested product cannot be found'
+ * @param {import('express').Request} req
+ * @param {import('express').Response} res
+ * @returns {Promise<void>}
+ *
+ */
+ const editProduct = async (req, res) => {
+  const schema = Joi.object({
+    name: Joi.string().max(255).optional(),
+    description: Joi.string().optional(),
+    price: Joi.number().precision(2).optional(),
+    imageFile: Joi.string().optional()
+  });
+
+  try {
+    const productId = parseInt(req.params.id, 10);
+    if (Number.isNaN(productId)) return res.status(404).end();
+
+    const product = await Product.findByPk(productId);
+    if (!product) return res.status(404).end();
+
+    let newProductValues = await schema.validateAsync(req.fields);
+
+    if (req.files && req.files.imageFile) {
+      const imagePath = req.files.imageFile.path,
+        imageName = path.basename(imagePath);
+      newProductValues = {
+        ...newProductValues,
+        image: imageName
+      }
+    }
+
+    for (const key in newProductValues) {
+      product[key] = newProductValues[key];
+    }
+
+    await product.save();
+
+    res.status(200).json(product);
+  } catch (err) {
+    if (err.details) { // body validation error
+      res.status(422).json({
+        errors: err.details
+      });
+    } else {
+      res.status(500).json({
+        error: err.message
+      });
+    }
+  }
+}
+
+/**
+ * Delete a product
+ * @swagger
+ * /api/products/{id}:
+ *  delete:
+ *    tags:
+ *      - Products
+ *    summary: Delete a product
+ *    parameters:
+ *      - in: path
+ *        name: id
+ *        schema:
+ *          type: integer
+ *        required: true
+ *        description: 'ID of the product to delete'
+ *    responses:
+ *      '204':
+ *        description: 'Product deleted successfully'
+ *      '404':
+ *        description: 'The requested product cannot be found'
+ * @param {import('express').Request} req
+ * @param {import('express').Response} res
+ * @returns {Promise<void>}
+ *
+ */
+ const deleteProduct = async (req, res) => {
+  try {
+    const productId = parseInt(req.params.id, 10);
+    if (Number.isNaN(productId)) return res.status(404).end();
+
+    const product = await Product.findByPk(productId);
+    if (!product) return res.status(404).end();
+
+    await product.destroy();
+
+    res.status(204).end();
+  } catch (err) {
+    console.error(err);
+  }
 }
 
 module.exports = {
-  createProduct
+  createProduct,
+  getAllProducts,
+  getProduct,
+  editProduct,
+  deleteProduct
 }
--- src/routes/api.js
+++ src/routes/api.js
@@ -3,9 +3,10 @@ const express = require('express'),
 
 const ProductsController = require('../controllers/ProductsController');
 
-router.get('/products',
-ProductsController.createProduct);
-
 router.post('/products', ProductsController.createProduct);
+router.get('/products', ProductsController.getAllProducts);
+router.get('/products/:id', ProductsController.getProduct);
+router.patch('/products/:id', ProductsController.editProduct);
+router.delete('/products/:id', ProductsController.deleteProduct);
 
 module.exports = router;
